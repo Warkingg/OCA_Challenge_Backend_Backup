8a10fd3ce31f1fcfd7753c3221c9fa79e4cf829f
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-05-21 10:11:10 +0700

    Add User Entity Attributes

--- src/main/java/model/Topic.java
+++ src/main/java/model/Topic.java
@@ -1,4 +1,13 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Topic {
+
 }
